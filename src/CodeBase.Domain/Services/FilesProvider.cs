3d592be8d9a2bc804aa59aec1a6d11c1409654d9
Gluschenko <qugluschenko@gmail.com>
2020-11-27 23:20:44 +0300

    Draft FilesProvider

---
+++ src/CodeBase.Domain/Services/FilesProvider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace CodeBase.Domain.Services
+{
+    public class FilesProvider
+    {
+        public FilesProvider()
+        {
+
+        }
+
+        public IEnumerable<string> GetFilesList(string basePath, string[] filters) 
+        {
+            // cd basePath
+            // git ls-tree --full-tree -r --name-only HEAD
+
+            return new List<string>();
+        }
+    }
+}
