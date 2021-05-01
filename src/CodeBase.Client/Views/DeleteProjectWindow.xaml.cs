50c8370e58ec34591b717d7a58c7c2fd2e90dfa9
Alexander Gluschenko <qugluschenko@gmail.com>
2021-05-01 15:31:50 +0300

    Modern: DeleteProjectWindow

---
+++ src/CodeBase.Client/Views/DeleteProjectWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace CodeBase.Client.Views
+{
+    public partial class DeleteProjectWindow : Window
+    {
+        public DeleteProjectWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void YesButtonClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void NoButtonClick(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
