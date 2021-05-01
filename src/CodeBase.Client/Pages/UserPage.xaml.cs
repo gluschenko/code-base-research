7ba44e08590df24616fd230d74b11083c2043265
Alexander Gluschenko <qugluschenko@gmail.com>
2021-05-01 16:38:38 +0300

    Modern: add UserPage

---
+++ src/CodeBase.Client/Pages/UserPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CodeBase.Client.Pages
+{
+    public partial class UserPage : Page
+    {
+        public UserPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Logout_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
