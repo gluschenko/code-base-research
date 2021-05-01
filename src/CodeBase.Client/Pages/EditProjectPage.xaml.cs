2bf9139e2311be64df540ae8ae3da1392667ca54
Alexander Gluschenko <qugluschenko@gmail.com>
2021-05-01 16:20:15 +0300

    Modern: add EditProjectPage

---
+++ src/CodeBase.Client/Pages/EditProjectPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CodeBase.Client.Pages
+{
+    public partial class EditProjectPage : Page
+    {
+        public EditProjectPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void PathTextBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+        }
+
+        private void ProjectColor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+    }
+}
