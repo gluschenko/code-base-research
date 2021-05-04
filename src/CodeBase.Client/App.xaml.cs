6cac62f16e3f777485ac551d8b2db712da19e23e
Alexander Gluschenko <qugluschenko@gmail.com>
2021-05-04 22:28:55 +0300

    Modern: run once checking

--- src/CodeBase.Client/App.xaml.cs
+++ src/CodeBase.Client/App.xaml.cs
@@ -1,8 +1,30 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+using CodeBase.Domain.Services;
 
 namespace CodeBase.Client
 {
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            if (!IsRunningOnce())
+            {
+                var currentProcess = Process.GetCurrentProcess();
+                MessageHelper.Warning($"{currentProcess.ProcessName} is already running! Close all instances and try again");
+                Shutdown();
+            }
+        }
+
+        private static bool IsRunningOnce()
+        {
+            var processList = Process.GetProcesses();
+            var current = Process.GetCurrentProcess();
+
+            return !processList.Any(x => x.ProcessName == current.ProcessName && x.Id != current.Id);
+        }
     }
 }
--- src/CodeBase/App.xaml.cs
+++ src/CodeBase/App.xaml.cs
@@ -2,9 +2,6 @@
 
 namespace CodeBase
 {
-    /// <summary>
-    /// Логика взаимодействия для App.xaml
-    /// </summary>
     public partial class App : Application
     {
         public const string DefaultReceiverURL = "http://codebase.gluschenko.net/api/";
