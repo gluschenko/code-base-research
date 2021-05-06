c269ef99e015c83794b1b3adb503700feb382a48
Alexander Gluschenko <qugluschenko@gmail.com>
2021-05-06 14:38:32 +0300

    Modern: PageHeader instead of PageWrapper

---
+++ src/CodeBase.Client/Controls/PageHeader.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CodeBase.Client.Controls
+{
+    public partial class PageHeader : UserControl
+    {
+        public static readonly DependencyProperty TitlePropery = 
+            DependencyProperty.Register(nameof(Title), typeof(string), typeof(PageHeader), new PropertyMetadata(OnTitleChanged));
+
+        public string Title 
+        {
+            get => GetValue(TitlePropery)?.ToString();
+            set => SetValue(TitlePropery, value); 
+        }
+
+        public Visibility TitleVisibility { get; set; } = Visibility.Visible;
+
+        public PageHeader()
+        {
+            InitializeComponent();
+        }
+
+        private static void OnTitleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var item = d as PageHeader;
+            item.TitleVisibility = !string.IsNullOrWhiteSpace(e.NewValue?.ToString()) ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
