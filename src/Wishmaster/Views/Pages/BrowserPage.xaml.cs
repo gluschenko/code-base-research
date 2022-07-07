2638df7fc2164f5f67084b8d4c3b310a581a7e93
Alexander Gluschenko <qugluschenko@gmail.com>
2022-07-08 01:41:04 +0300

    BrowserPage

--- src/Wishmaster/Views/Pages/BrowserPage.xaml.cs
+++ src/Wishmaster/Views/Pages/BrowserPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows.Controls;
 
 namespace Wishmaster.Views.Pages
 {
@@ -7,6 +8,41 @@ namespace Wishmaster.Views.Pages
         public BrowserPage()
         {
             InitializeComponent();
+            Loaded += BrowserPage_Loaded;
+        }
+
+        private void BrowserPage_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            WebView.Source = new Uri(AddressTextBox.Text);
+        }
+
+        private void GoButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            try
+            {
+                var url = AddressTextBox.Text;
+
+                if (string.IsNullOrWhiteSpace(url))
+                {
+                    return;
+                }
+
+                WebView.Source = new Uri(AddressTextBox.Text);
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private void WebView_NavigationStarting(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationStartingEventArgs e)
+        {
+            LoadingOverlay.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void WebView_NavigationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e)
+        {
+            LoadingOverlay.Visibility = System.Windows.Visibility.Hidden;
         }
     }
 }
---
+++ src/Wishmaster/Views/Shared/WebView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wishmaster.Views.Shared
+{
+    public partial class WebView : UserControl
+    {
+        public WebView()
+        {
+            InitializeComponent();
+            Loaded += WebView_Loaded;
+        }
+
+        private async void WebView_Loaded(object sender, RoutedEventArgs e)
+        {
+            await WebViewElement.EnsureCoreWebView2Async();
+        }
+    }
+}
