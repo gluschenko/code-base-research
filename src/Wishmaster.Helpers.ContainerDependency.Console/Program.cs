244846b6f9341132cf39889fadc8068b30916e32
Alexander Gluschenko <qugluschenko@gmail.com>
2022-07-19 01:30:53 +0300

    Graph: nodes and edges

--- src/Wishmaster.Helpers.ContainerDependency.Console/Program.cs
+++ src/Wishmaster.Helpers.ContainerDependency.Console/Program.cs
@@ -1,12 +1,41 @@
-﻿namespace Wishmaster.Helpers.ContainerDependency.Console
+﻿using Wishmaster.Helpers.ContainerDependency.Models;
+
+namespace Wishmaster.Helpers.ContainerDependency.Console
 {
     public static class Program
     {
         public static int Main()
         {
+            var graph = new Graph<NodeData, EdgeData>();
 
+            var a = graph.AddNode(new NodeData("1", "1", "1", "1", "1", "1", 1, "1"));
+            var b = graph.AddNode(new NodeData("2", "2", "2", "2", "2", "2", 2, "2"));
+            var c = graph.AddNode(new NodeData("3", "3", "3", "3", "3", "3", 3, "3"));
+
+            graph.SetEdge(a, b, new EdgeData(EdgeType.ProjectReference, ""));
+            graph.SetEdge(b, c, new EdgeData(EdgeType.ProjectReference, ""));
+            graph.SetEdge(c, a, new EdgeData(EdgeType.ProjectReference, ""));
 
             return 0;
         }
+    }
+
+    public record NodeData(
+        string Project,
+        string Solution,
+        string Repository,
+        string PackageId,
+        string Platform,
+        string LangVersion,
+        int Size,
+        string Color
+    );
+
+    public record EdgeData(EdgeType Type, string Text);
+
+    public enum EdgeType
+    {
+        PackageReference = 1,
+        ProjectReference = 2,
     }
 }
--- src/Wishmaster.Helpers.ContainerDependency/Models/Graph.cs
+++ src/Wishmaster.Helpers.ContainerDependency/Models/Graph.cs
@@ -9,22 +9,66 @@ namespace Wishmaster.Helpers.ContainerDependency.Models
     public class Graph<TNode, TEdge>
     {
         public Guid Uid { get; private set; }
-        private readonly List<Node<TNode>> _nodes;
-        private readonly List<Edge<TEdge>> _edges;
+        private readonly Dictionary<NodeIdentity, Node<TNode>> _nodes;
+        private readonly Dictionary<EdgeIdentity, Edge<TEdge>> _edges;
 
         public Graph()
         {
             Uid = Guid.NewGuid();
-            _nodes = new List<Node<TNode>>();
-            _edges = new List<Edge<TEdge>>();
+            _nodes = new Dictionary<NodeIdentity, Node<TNode>>();
+            _edges = new Dictionary<EdgeIdentity, Edge<TEdge>>();
         }
 
-        public void AddNode(Node<TNode> node)
+        public Node<TNode> AddNode(TNode value)
+        {
+            return AddNode(new Node<TNode>(value));
+        }
+
+        public Node<TNode> AddNode(Node<TNode> node)
+        {
+            if (node is null)
+            {
+                throw new ArgumentNullException(nameof(node));
+            }
+
+            var identity = new NodeIdentity(node.Uid);
+
+            if (_nodes.ContainsKey(identity))
+            {
+                throw new Exception($"Node {node.Uid} is already in Graph");
+            }
+
+            _nodes[identity] = node;
+            return node;
+        }
+
+        public void RemoveNode()
         {
 
         }
 
-        public void SetEdge()
+        public void SetEdge(Node<TNode> from, Node<TNode> to, TEdge value)
+        {
+            if (from is null)
+            {
+                throw new ArgumentNullException(nameof(from));
+            }
+
+            if (to is null)
+            {
+                throw new ArgumentNullException(nameof(to));
+            }
+
+            SetEdge(from.Uid, to.Uid, value);
+        }
+
+        public void SetEdge(Guid from, Guid to, TEdge value)
+        {
+            var identity = new EdgeIdentity(from, to);
+            _edges[identity] = new Edge<TEdge>(value, from, to);
+        }
+
+        public void RemoveEdge()
         {
 
         }
@@ -33,10 +77,32 @@ namespace Wishmaster.Helpers.ContainerDependency.Models
         {
             var hash = new HashCode();
 
-            _nodes.ForEach(x => hash.Add(x));
-            _edges.ForEach(x => hash.Add(x));
+            foreach (var item in _nodes.Values)
+            {
+                hash.Add(item);
+            }
+
+            foreach (var item in _edges.Values)
+            {
+                hash.Add(item);
+            }
 
             return hash.ToHashCode();
         }
     }
+
+    internal record struct NodeIdentity(Guid Uid)
+    {
+        public static implicit operator NodeIdentity(Guid uid)
+        {
+            return new NodeIdentity(uid);
+        }
+
+        public static implicit operator Guid(NodeIdentity id)
+        {
+            return id.Uid;
+        }
+    }
+
+    internal record struct EdgeIdentity(Guid From, Guid To);
 }
