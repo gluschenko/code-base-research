844691283138f59af4def0877b6f7380e50d21cd
Alexander Gluschenko <qugluschenko@gmail.com>
2022-07-24 21:41:55 +0300

    NavLinkViewModel

---
+++ src/Wishmaster/ViewModels/AppDataViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TypeGen.Core.TypeAnnotations;
+
+namespace Wishmaster.ViewModels
+{
+    [ExportTsInterface]
+    public class AppDataViewModel
+    {
+        public IEnumerable<NavLinkViewModel>? SidebarNavigation { get; set; }
+    }
+
+    [ExportTsInterface]
+    public class NavLinkViewModel
+    {
+        public string Text { get; set; }
+        public string Url { get; set; }
+
+        public NavLinkViewModel(string text, string url)
+        {
+            Text = text;
+            Url = url;
+        }
+    }
+}
