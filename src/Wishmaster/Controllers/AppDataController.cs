8463cfa7cc8eebb5adafeeac67abd0cd270c9468
Alexander Gluschenko <qugluschenko@gmail.com>
2022-07-24 20:23:09 +0300

    AppDataController

---
+++ src/Wishmaster/Controllers/AppDataController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Wishmaster.Controllers
+{
+    [Route("api")]
+    public class AppDataController : Controller
+    {
+        public AppDataController()
+        {
+
+        }
+
+        [HttpGet("get-app-data")]
+        public IActionResult GetAppData()
+        {
+            return Json(new
+            {
+                Asd = 1,
+            });
+        }
+    }
+}
